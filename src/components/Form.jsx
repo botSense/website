fa98ce4094660baf3bdb7afa9fb194eb53056bb6
dhanush <dhanushshetty201@gmail.com>
2023-01-13 14:40:45 +0530

    Font Changed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,10 +5,10 @@ const Form = () => {
   return (
     <div
       name="internship"
-      className="w-full h-[75vh] bg-[#0d0e0e] text-gray-300 flex justify-center items-center"
+      className="w-full h-[75vh] bg-[#bfbfbf] flex justify-center items-center"
     >
       <iframe
-        className="w-full sm:w-[90%] h-full"
+        className="w-full sm:w-[90%] h-full p-4"
         src="https://docs.google.com/forms/d/e/1FAIpQLScxi3wVMNZItvzEIDW7TeujCmxuuiZjkYwAA_-9Es1V9yd1lw/viewform?embedded=true"
       >
         Loading…
