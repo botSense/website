fc61078364c5a29a3cdbeec38dc456d1e52d57a4
botSense <tbotsense@gmail.com>
2024-06-22 01:14:27 +0530

    Better the before

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 // import { Link } from 'react-scroll';
 // import ignitra from "../assests/ignitra.jpg";
-import botsense from '../assests/botsense.JPG';
+import botsense from "../assests/botsense-internship.jpeg";
 // import IMG2 from "../assests/img2.jpg";
 // import IMG3 from "../assests/img3.jpg";
 // import IMG4 from "../assests/img4.jpg";
-import { Fade } from 'react-awesome-reveal';
+import {Fade} from "react-awesome-reveal";
 
 const Home = () => {
-	return (
-		<Fade>
-			<div name='home' className='md:h-full h-[50vh] w-full relative'>
-				<img src={botsense} className='h-vh w-screen brightness-50 ' alt='' />
-			</div>
-		</Fade>
-	);
+  return (
+    <Fade>
+      <div name='home' className='md:h-full h-[50vh] w-full relative'>
+        <img src={botsense} className='h-vh w-screen brightness-50 ' alt='' />
+      </div>
+    </Fade>
+  );
 };
 
 export default Home;
